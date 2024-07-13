897c12b14c7a68f9da8ca3335b979c1f9784584a
Pratiksha2356 <pratikshashettypawar545@gmail.com>
2024-07-13 20:47:50 +0530

    Add files via upload

---
+++ Backend/src/db.js
@@ -0,0 +1,11 @@
+const {Pool} = require('pg')
+
+//Configurations for the database connection
+//There should be changed according the deployed platform
+exports.pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mail_system',
+    password: '58132134',
+    port: 5432,
+})
